630dc8dfa41fb9add99dd95905b4eb6db41b8af6
joaoreis7 <142408632+joaoreis7@users.noreply.github.com>
2023-08-30 13:14:51 +0000

    aula02

---
+++ aulas/aula02/variaveis.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+.int main() {
+
+  char caracter; // 'a','b', 'c', -128 a 127 - 1 byte
+
+  unsigned char caracter_sem_sinal; // 0 a 255
+
+  char string[10]; // "um texto"
+  
+  int inteiro; // -2147483648 a 2147483647 - 4 bytes
+
+  unsigned int inteiro_sem_sinal; // 0 a 4294967295
+
+  short int inteiro_curto; // -32768 a 32767 - 2 bytes
+
+  unsigned short int inteiro_curto_sem_sinal; // 0 a 65535
+  
+  float flutuante; // 0.123456f - 4 bytes
+
+  double duplo; // 0.123456789012345 - 8bytes
+  
+  void nada; // 1 byte
+  
+  return, 0
+}
---
+++ praticas/pratica02/questao2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int base = 10;
+  int altura = 2;
+
+  int area = base * altura/2;
+  
+  printf("A area é %i\n", area);
+  return 0;
+}
